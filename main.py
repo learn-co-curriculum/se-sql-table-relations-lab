3f11df00f08d4aa51641af0f06eb7d132cce4744
stevekahanya <wakahusteve@gmail.com>
2026-05-29 11:52:55 +0300

    step 5

--- main.py
+++ main.py
@@ -43,9 +43,13 @@ df_contacts = pd.read_sql("""
     ORDER BY c.contactLastName;
 """, conn)
 
-# STEP 5
-# Replace None with your code
-df_payment = None
+# STEP 5: Customer contacts, payment amounts, and dates of payment. Sorted DESC by amount. (Hint used: CAST amount to numeric).
+df_payment = pd.read_sql("""
+    SELECT c.contactFirstName, c.contactLastName, p.amount, p.paymentDate 
+    FROM customers c 
+    JOIN payments p ON c.customerNumber = p.customerNumber 
+    ORDER BY CAST(p.amount AS REAL) DESC;
+""", conn)
 
 # STEP 6
 # Replace None with your code
