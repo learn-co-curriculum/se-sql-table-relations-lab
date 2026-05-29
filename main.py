2740e243ea0736751cd7a045776660d32d98d6c5
stevekahanya <wakahusteve@gmail.com>
2026-05-29 11:52:25 +0300

    step 4

--- main.py
+++ main.py
@@ -34,9 +34,14 @@ df_employee = pd.read_sql("""
     ORDER BY e.firstName, e.lastName;
 """, conn)
 
-# STEP 4
-# Replace None with your code
-df_contacts = None
+# STEP 4: Customer contact info and sales rep employee number for customers who haven't placed an order. Sorted by contact's last name.
+df_contacts = pd.read_sql("""
+    SELECT c.contactFirstName, c.contactLastName, c.phone, c.salesRepEmployeeNumber 
+    FROM customers c 
+    LEFT JOIN orders o ON c.customerNumber = o.customerNumber 
+    WHERE o.orderNumber IS NULL 
+    ORDER BY c.contactLastName;
+""", conn)
 
 # STEP 5
 # Replace None with your code
